6e5aeb5e8c5b502271ac8084fd9786cf1e45111d
Artur Kempny <artur.kempny@gmail.com>
2020-02-04 14:41:13 +0100

    Add Scroller class and use it

---
+++ Scroller.js
@@ -0,0 +1,50 @@
+class Scroller {
+  constructor(rootSelector) {
+    const rootElement = document.querySelector(rootSelector);
+    this.sections = document.querySelectorAll("section");
+    const sectionsArr = [...this.sections];
+
+    const currentSectionIndex = sectionsArr.findIndex(this.isScrolledIntoView);
+    this.currentSectionIndex = Math.max(currentSectionIndex, 0);
+    this.isThrottled = false;
+  }
+  isScrolledIntoView(el) {
+    const rect = el.getBoundingClientRect();
+    const elemTom = rect.top;
+    const elemBottom = Math.floor(rect.bottom);
+
+    const isVisible = elemTom >= 0 && elemBottom <= window.innerHeight;
+    return isVisible;
+  }
+  listenScroll = event => {
+    if (this.isThrottled) return;
+    this.isThrottled = true;
+
+    setTimeout(() => {
+      this.isThrottled = false;
+    }, 500);
+
+    const direction = event.wheelDelta < 0 ? 1 : -1;
+    this.scroll(direction);
+  };
+
+  scroll = direction => {
+    if (direction === 1) {
+      const isLastSection =
+        this.currentSectionIndex === this.sections.length - 1;
+      if (isLastSection) return;
+    } else if (direction === -1) {
+      const firstSection = this.currentSectionIndex === 0;
+      if (firstSection) return;
+    }
+
+    this.currentSectionIndex = this.currentSectionIndex + direction;
+    this.scrollToCurrentSection();
+  };
+  scrollToCurrentSection = () => {
+    this.sections[this.currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  };
+}
